214e99e178c7d5cc8f33a595ae042c2bc2e7eb57
Matthieu Mota <matthieumota@gmail.com>
2023-08-21 17:19:02 +0200

    Begin card

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps(['user'])
+
+import Button from '../components/Button.vue'
+
+const isOpen = true;
+</script>
+
+<template>
+  <div class="card">
+    <img :src="user.avatar" :alt="user.name" />
+    <h2>{{ user.name }}</h2>
+    <p>{{ user.job }}</p>
+    <Button>Voir biographie</Button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  background-color: #fff;
+  padding: 16px;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px 0 #0000001a;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+import Card from '../components/Card.vue'
+
+const team = ref([
+  {
+    id: 1,
+    name: 'Fiorella',
+    job: 'Manager',
+    avatar: 'https://i.pravatar.cc/150?u=fiorella@boxydev.com',
+    biography:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam mollitia voluptatum sunt placeat aliquam iusto exercitationem at? Vel itaque porro quis saepe quod soluta alias facere eum deleniti? Rem, eveniet.'
+  },
+  {
+    id: 2,
+    name: 'Matthieu',
+    job: 'Développeur',
+    avatar: 'https://i.pravatar.cc/150?u=matthieu@boxydev.com',
+    biography:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam mollitia voluptatum sunt placeat aliquam iusto exercitationem at? Vel itaque porro quis saepe quod soluta alias facere eum deleniti? Rem, eveniet.'
+  },
+  {
+    id: 3,
+    name: 'Marina',
+    job: 'Commerciale',
+    avatar: 'https://i.pravatar.cc/150?u=marina@boxydev.com',
+    biography:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam mollitia voluptatum sunt placeat aliquam iusto exercitationem at? Vel itaque porro quis saepe quod soluta alias facere eum deleniti? Rem, eveniet.'
+  }
+])
+</script>
+
 <template>
   <h1 class="title">A propos</h1>
+
+  <div class="flex">
+    <Card v-for="user in team" :key="user.id" :user="user" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+$itemNumber: 3;
+
+.flex {
+  gap: 30px;
+  flex-wrap: wrap;
+
+  .card {
+    width: calc(100% / $itemNumber - (30px * ($itemNumber - 1) / $itemNumber));
+  }
+}
+</style>
