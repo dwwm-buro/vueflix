8c814f48186f8648da9f7328ee67bf14c720d712
Matthieu Mota <matthieumota@gmail.com>
2023-08-23 17:00:10 +0200

    wip single movie

--- src/views/MovieShowView.vue
+++ src/views/MovieShowView.vue
@@ -7,10 +7,22 @@ const movie = ref({})
 getMovie(route.params.id).then(response => {
   movie.value = response
 })
+const color = ref([0, 0, 0])
 </script>
 
 <template>
+  <div class="movie-show" :style="{ backgroundImage: `url(${movie.backdrop_path})` }">
+    <div class="movie-background" :style="{ backgroundColor: `rgba(${color}, 0.75)` }">
+      <div class="container">
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.movie-show {
+  background-position: center;
+  background-size: cover;
+}
+
 </style>
